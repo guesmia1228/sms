5828faddb567059b6beaafd77c2f4c7d28037d69
Guesmia Abdelmadjid <guesmiaabdelmadjid6@gmail.com>
2021-09-09 23:27:51 +0300

    test and factory

--- spec/controllers/students_controller_spec.rb
+++ spec/controllers/students_controller_spec.rb
@@ -3,9 +3,11 @@ require "rails_helper"
 RSpec.describe StudentsController, type: :controller do
   describe "GET #index" do
     before { get :index }
+    let!(:student_01) { create(:student) }
+    let!(:student_02) { create(:student) }
 
     it "If the controller correctly assigns @students" do
-      expect(assigns(:students)).to be_empty
+      expect(assigns(:students)).to eq [student_01, student_02]
     end
     it "renders the index template" do
       expect(response).to render_template(:index)
