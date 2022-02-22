b93d38049c46f515d047158ecfd6cce67994effd
Guesmia Abdelmadjid <guesmiaabdelmadjid6@gmail.com>
2022-02-22 23:04:48 +0300

    controller test on new and create actions

--- spec/controllers/students_controller_spec.rb
+++ spec/controllers/students_controller_spec.rb
@@ -13,4 +13,42 @@ RSpec.describe StudentsController, type: :controller do
       expect(response).to render_template(:index)
     end
   end
+
+  describe "GET #new" do
+    before {get :new}
+
+    it "Instance not saved in the bank" do
+      expect(assigns(:student)).not_to be_persisted
+    end
+    it "renders the new template" do
+      expect(response).to render_template(:new)
+    end
+  end
+
+  describe "POST #create" do 
+    let(:action) {post :create, params: {student: params}}
+
+    context "success" do
+      let(:params) { {name: "Anderson"} }
+
+      it "Redirection to Students page" do
+        action
+        expect(response).to redirect_to students_path
+      end
+      it "Student was saved" do
+        # posso usar para deletar um estudante usando -1
+        expect {action}.to change{Student.count}.by 1
+      end
+    end
+    context "fail" do
+      let(:params) { {name: nil} }
+      it "renders the new template" do
+        action
+        expect(response).to render_template(:new)
+      end
+    end
+    
+  end
+
+
 end
