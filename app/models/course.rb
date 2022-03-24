34d00ad2735dba06631364ace31ec4fe98e90f39
Guesmia Abdelmadjid <guesmiaabdelmadjid6@gmail.com>
2022-03-24 20:11:45 +0300

    generate random number indentifier

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,16 @@
 class Course < ApplicationRecord
   has_many :student_courses
   has_many :students, through: :student_courses
+
+  def generate_random_number_indentifier
+    number_generate = random_number
+    while Course.exists?(indentifier: number_generate)
+      number_generate = random_number
+    end
+    self.indentifier = number_generate
+  end
+
+  def random_number
+    "#{rand(0..1000000)}"
+  end
 end
---
+++ spec/models/course_spec.rb
@@ -0,0 +1,12 @@
+RSpec.describe Course, type: :model do
+  describe 'validations' do
+    describe '#name' do
+      before { subject.valid? }
+      context 'When the name is nil' do
+        it 'an error must be associated with the name' do
+          expect(subject.errors[:name]).not_to be_empty
+        end
+      end
+    end
+  end
+end
