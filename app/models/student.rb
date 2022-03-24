ea0069a59bd224d38ec99fc6f179c57e00b2c690
Guesmia Abdelmadjid <guesmiaabdelmadjid6@gmail.com>
2022-03-24 19:38:18 +0300

    creating the relationship between students and courses

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,6 +2,9 @@ class Student < ApplicationRecord
   validates :name, presence: true
   before_save :generate_random_number_registration
 
+  has_one :student_courses
+  has_one :course, through: :student_courses
+
   def generate_random_number_registration
     number_generate = random_number
     while Student.exists?(registration: number_generate)
