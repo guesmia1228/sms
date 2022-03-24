baee3b0dc873f48fd663ff1495378c1cf6589d99
Guesmia Abdelmadjid <guesmiaabdelmadjid6@gmail.com>
2022-03-24 22:14:47 +0300

    update

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,18 +1,13 @@
 class Course < ApplicationRecord
+  include GenerateRandomNumber
   validates :name, presence: true
-  before_save :generate_random_number_indentifier
+  before_save :generate_random_number
 
   has_many :student_courses
   has_many :students, through: :student_courses
 
-  def generate_random_number_indentifier
-    number_generate = random_number
-    while Course.exists?(indentifier: number_generate)
-      number_generate = random_number
-    end
-    self.indentifier = number_generate
-  end
-
+  private
+  
   def random_number
     "#{rand(0..1000000)}"
   end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,18 +1,13 @@
 class Student < ApplicationRecord
+  include GenerateRandomNumber
   validates :name, presence: true
-  before_save :generate_random_number_registration
+  before_save :generate_random_number
 
   has_one :student_courses
   has_one :course, through: :student_courses
 
-  def generate_random_number_registration
-    number_generate = random_number
-    while Student.exists?(registration: number_generate)
-      number_generate = random_number
-    end
-    self.registration = number_generate
-  end
-
+  private
+  
   def random_number
     "#{Date.today.year}-#{rand(0..1000000)}"
   end
