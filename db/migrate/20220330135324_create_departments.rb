67b327582106bcded8ff557e579c78f6a3bf1272
Guesmia Abdelmadjid <guesmiaabdelmadjid6@gmail.com>
2022-03-30 20:42:27 +0300

    update migration

--- db/migrate/20220330135324_create_departments.rb
+++ db/migrate/20220330135324_create_departments.rb
@@ -3,8 +3,7 @@ class CreateDepartments < ActiveRecord::Migration[6.1]
     create_table :departments do |t|
       t.string :name
       t.string :indentifier
-      t.references :department, foreign_key: true
-
+      
       t.timestamps
     end
   end
