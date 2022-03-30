2e4f545238e9d3421793bca84ace8c124bdf8dcd
Guesmia Abdelmadjid <guesmiaabdelmadjid6@gmail.com>
2022-03-30 22:20:37 +0300

    update

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -21,8 +21,8 @@ class DepartmentsController < ApplicationController
   end
 
   def update
-    department = Department.find(params[:id])
-    if department.update(department_params)
+    @department = Department.find(params[:id])
+    if @department.update(department_params)
       redirect_to departments_path
     else
       render :edit
