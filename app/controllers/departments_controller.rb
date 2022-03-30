cc8f901ee2f8989c1a00ca0b84842f29646882de
Guesmia Abdelmadjid <guesmiaabdelmadjid6@gmail.com>
2022-03-30 20:54:44 +0300

    department new

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -2,4 +2,23 @@ class DepartmentsController < ApplicationController
   def index
     @departments = Department.all
   end
+
+  def new
+    @department = Department.new
+  end
+
+  def create
+    @department = Department.new(department_params)
+    if @department.save
+      redirect_to departments_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def department_params
+    params.require(:department).permit(:name, :indentifier)
+  end
 end
