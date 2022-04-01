b4d22fe2c71a9418d4414190feb55629f211b9d6
Guesmia Abdelmadjid <guesmiaabdelmadjid6@gmail.com>
2022-04-01 19:49:27 +0300

    has_many

--- app/models/college_subject.rb
+++ app/models/college_subject.rb
@@ -2,4 +2,7 @@ class CollegeSubject < ApplicationRecord
   include GenerateRandomNumber
   validates :name, presence: true
   before_save :generate_random_number
+
+  has_many :couse_subject
+  has_many :course, through: :couse_subject
 end
