112d9cb6b7ee1a9ff404ad28ab9f4e26ad0799da
Guesmia Abdelmadjid <guesmiaabdelmadjid6@gmail.com>
2022-04-02 19:47:06 +0300

    update Course

--- app/models/course.rb
+++ app/models/course.rb
@@ -6,6 +6,6 @@ class Course < ApplicationRecord
   has_many :student_courses
   has_many :students, through: :student_courses
 
-  has_many :couse_subject
-  has_many :college_subject, through: :couse_subject
+  has_many :couse_subjects
+  has_many :college_subjects, through: :couse_subjects
 end
