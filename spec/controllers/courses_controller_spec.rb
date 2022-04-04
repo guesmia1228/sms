32a640660adb1d57ad27518f43948e4ff26929a9
Guesmia Abdelmadjid <guesmiaabdelmadjid6@gmail.com>
2022-04-04 20:32:36 +0300

    CoursesController test

--- spec/controllers/courses_controller_spec.rb
+++ spec/controllers/courses_controller_spec.rb
@@ -13,4 +13,56 @@ RSpec.describe CoursesController, type: :controller do
       expect(response).to render_template(:index)
     end
   end
+
+  describe "GET #new" do
+    before {get :new}
+
+    it "Instance not saved in the bank" do
+      expect(assigns(:course)).not_to be_persisted
+    end
+    
+    it "renders the new template" do
+      expect(response).to render_template(:new)
+    end
+  end
+
+  describe "POST #create" do 
+    let(:action) {post :create, params: {course: params}}
+
+    context "success" do
+      let(:params) { {name: "Computation"} }
+
+      it "Redirection to Course page" do
+        action
+        expect(response).to redirect_to courses_path
+      end
+      it "Course was saved" do
+        expect {action}.to change{Course.count}.by 1
+      end
+    end
+    context "fail" do
+      let(:params) { {name: nil} }
+      it "renders the new template" do
+        action
+        expect(response).to render_template(:new)
+      end
+    end
+  end
+
+  describe "DELETE #destroy" do
+    let!(:course) { create(:course) }
+    let(:action) {delete :destroy, params: id}
+
+    context "success" do
+      let(:id) { {id: course.id} }
+
+      it "deleted course" do
+        expect {action}.to change{Course.count}.by -1
+      end
+      it "Redirection to Course pag" do
+        action
+        expect(response).to redirect_to courses_path
+      end
+    end
+  end
 end
